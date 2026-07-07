0a59404cda37412f4dbba3e7abe60c2974291a57
Aniket Baidya <aniketbaidyarocks@gmail.com>
2026-07-07 15:31:03 +0100

    Add variable declaration examples in Go
    Added examples demonstrating different variable declaration patterns in Go, including:
    - Package-level variable and constant declarations
    - Default value initialization for uninitialized variables
    - Implicit type inference
    - Short variable declaration syntax using :=
    
    These examples illustrate various ways to declare and initialize variables in Go.

--- 02variables/main.go
+++ 02variables/main.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+var jwtToken = 300000
+
+// LoginToken public since first character is capital
+const LoginToken = "ghabbb"
+
 func main() {
 	var username string = "aniket"
 	fmt.Println(username)
@@ -15,4 +20,16 @@ func main() {
 	fmt.Println(smallVal)
 	fmt.Printf("Variable is  of type %T\n", smallVal)
 
+	// default values and some aliases
+	var variable int
+	fmt.Println(variable)
+	fmt.Printf("Variable is  of type %T\n", variable)
+
+	// implicit
+	var website = "learncodeonline"
+	fmt.Println(website)
+
+	// no var style
+	numberOfUsers := 30000.0
+	fmt.Println(numberOfUsers)
 }
